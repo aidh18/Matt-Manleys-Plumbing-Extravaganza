9d60851766eb2513e12d9b6fd73c9f724b01b8ad
Aidan <jaidanstacey@gmail.com>
2022-07-06 13:37:26 -0600

    Created folders and stuff

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Matt_Manleys_Plumbing_Extravaganza.Game.Directing;
+
+namespace Matt_Manleys_Plumbing_Extravaganza
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+            director.StartGame();
+        }
+    }
+}
