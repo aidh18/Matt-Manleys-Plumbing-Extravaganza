6b6d31270e4378c5ab42fe8a22222690c06c964f
Aidan <jaidanstacey@gmail.com>
2022-07-07 00:38:39 -0600

    Added a ton of stuff

---
+++ Game/Casting/Player.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Matt_Manleys_Plumbing_Extravaganza.Game.Casting
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Player : Actor
+    {
+        public Player(bool debug = false) : base(debug)
+        {
+            
+        }
+    }
+}
